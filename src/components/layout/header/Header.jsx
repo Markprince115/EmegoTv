61ab2b89e05f407fb15aef47f500b0d60b85c32a
{marlprince115} <{markprince29.mp@gmail.com}>
2025-10-02 07:27:25 +0100

    designed the hero section and also installedsome dependencies

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,9 +1,22 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div></div>
+    <header className='flex items-center justify-around bg-transparent relative z-50 p-4'>
+      {/* header logo or icon or text */}
+      <div className='text-center'>
+        <h1 className='text-3xl font-bold text-center text-neutral-50'>EMEGO <span><sup className='text-sm text-neutral-50'>TV</sup></span></h1>
+      </div>
+
+      {/* signup button */}
+      <div className='text-center'>
+        <Button className='px-7 py-2 rounded-full border bg-transparent'>SignUp</Button>
+      </div>
+    </header>
   )
 }
 
 export default Header
+
+
